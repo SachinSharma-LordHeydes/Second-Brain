00911ce14f15b471b37d1fe410a563b0c0956b17
MISTAKLEY <mistakley@localhost.localdomain>
2025-02-06 16:17:43 +0545

    new react project

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,26 @@
+// import { defineConfig } from 'vite'
+// export default defineConfig({
+  //   plugins: [
+    //     tailwindcss(),
+    //   ],
+    // })
+    
+    
+    
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  server: {
+    port: 3000
+  }
 })
