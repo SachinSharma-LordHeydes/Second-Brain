1a59bd968fe263b4b5a9bbd9ffda055a7a59e487
MISTAKLEY <mistakley@localhost.localdomain>
2025-02-06 16:24:50 +0545

    new react project

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,8 +10,8 @@ dotenv.config();
 
 const app=express();
 
-// app.use(cors());
-app.use(cors({ origin: "*" }));
+app.use(cors());
+// app.use(cors({ origin: "*" }));
 
 const PORT=process.env.PORT || 4000;
 dbConnect();
